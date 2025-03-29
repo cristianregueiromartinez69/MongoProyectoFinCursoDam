341b1a2e871948c06cc1b9e1a189841205c173d9
cristian <cregueiromartinez@danielcastelao.org>
2025-03-29 10:56:09 +0100

    añadiendo mongo al proyecto para historial y conteo

---
+++ MongoProyectoFinDam/src/main/java/com/norelationaldb/MongoProyectoFinDam/MongoProyectoFinDamApplication.java
@@ -0,0 +1,13 @@
+package com.norelationaldb.MongoProyectoFinDam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoProyectoFinDamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoProyectoFinDamApplication.class, args);
+	}
+
+}
