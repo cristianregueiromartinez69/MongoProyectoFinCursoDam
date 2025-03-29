4c007884e2a3b659557c06b80fb19ed7f813f100
cristian <cregueiromartinez@danielcastelao.org>
2025-03-29 11:27:23 +0100

    repositorio de mongo de usuarios

---
+++ MongoProyectoFinDam/src/main/java/com/norelationaldb/MongoProyectoFinDam/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.norelationaldb.MongoProyectoFinDam.repository;
+
+import com.norelationaldb.MongoProyectoFinDam.model.entity.Usuarios;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Repositorio de mongo para comunicarse con la base de datos para usuarios
+ * @author cristian && Joel
+ * @version 1.0
+ */
+public interface UsuarioRepository extends MongoRepository<Usuarios, String> {
+
+}
