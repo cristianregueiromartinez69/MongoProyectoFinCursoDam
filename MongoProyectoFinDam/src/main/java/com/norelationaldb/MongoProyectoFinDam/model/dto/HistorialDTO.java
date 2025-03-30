b14dbcaf5b0e10992cc142a9a09396bafcfcd176
cristian <cregueiromartinez@danielcastelao.org>
2025-03-30 10:41:05 +0200

    haciendo DTO del historial

--- MongoProyectoFinDam/src/main/java/com/norelationaldb/MongoProyectoFinDam/model/dto/HistorialDTO.java
+++ MongoProyectoFinDam/src/main/java/com/norelationaldb/MongoProyectoFinDam/model/dto/HistorialDTO.java
@@ -1,4 +1,49 @@
 package com.norelationaldb.MongoProyectoFinDam.model.dto;
 
 public class HistorialDTO {
+
+    private String id;
+    private Integer idCancion;
+    private String emailUser;
+
+    public HistorialDTO(String id, Integer idCancion, String emailUser) {
+        this.id = id;
+        this.idCancion = idCancion;
+        this.emailUser = emailUser;
+    }
+
+    public HistorialDTO(Integer idCancion, String emailUser) {
+        this.idCancion = idCancion;
+        this.emailUser = emailUser;
+    }
+
+    public HistorialDTO() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getIdCancion() {
+        return idCancion;
+    }
+
+    public void setIdCancion(Integer idCancion) {
+        this.idCancion = idCancion;
+    }
+
+    public String getEmailUser() {
+        return emailUser;
+    }
+
+    public void setEmailUser(String emailUser) {
+        this.emailUser = emailUser;
+    }
+
+
 }
