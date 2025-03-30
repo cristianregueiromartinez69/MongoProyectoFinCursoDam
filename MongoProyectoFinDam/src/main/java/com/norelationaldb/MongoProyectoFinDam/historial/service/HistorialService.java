a28a1a03410aadfbf8e6e84769a11c532ef838fe
cristian <cregueiromartinez@danielcastelao.org>
2025-03-30 10:44:46 +0200

    repositorio del historial

--- MongoProyectoFinDam/src/main/java/com/norelationaldb/MongoProyectoFinDam/historial/service/HistorialService.java
+++ MongoProyectoFinDam/src/main/java/com/norelationaldb/MongoProyectoFinDam/historial/service/HistorialService.java
@@ -1,4 +1,9 @@
 package com.norelationaldb.MongoProyectoFinDam.historial.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HistorialService {
+
+
 }
---
+++ MongoProyectoFinDam/src/main/java/com/norelationaldb/MongoProyectoFinDam/repository/HistorialRepository.java
@@ -0,0 +1,14 @@
+package com.norelationaldb.MongoProyectoFinDam.repository;
+
+import com.norelationaldb.MongoProyectoFinDam.model.entity.Historial;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repositorio del historial de canciones guardadas en mongo
+ * @author cristian && Joel
+ * @version 1.0
+ */
+@Repository
+public interface HistorialRepository extends MongoRepository<Historial, String> {
+}
